f26874fa52de13b8d5a23706c61a625707321161
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-01-17 13:42:27 -0300

    Added music data file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import React from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 
+// Import styles
 import "./styles/app.scss";
+
+// Import music data
+import data from "./data";
 
 function App() {
   return (

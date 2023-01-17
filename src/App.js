0e4b36d71e2c70898b5572bf4c3bbe3f1a10905c
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-01-17 13:06:21 -0300

    Added Player and Song components sass styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 
+import "./styles/app.scss";
+
 function App() {
   return (
     <div className="App">

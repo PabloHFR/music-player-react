866f709bb7a31a68bacaf6f2507a828c7d51545c
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-01-17 14:06:09 -0300

    Added play and pause feature

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
   faAngleLeft,
   faAngleRight,
+  faPause,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  // Ref
+  const audioRef = useRef(null);
+
+  // Event Handlers
+  const playAudioHandler = () => {
+    if (isPlaying) {
+      setIsPlaying(!isPlaying);
+      audioRef.current.pause();
+    } else {
+      setIsPlaying(!isPlaying);
+      audioRef.current.play();
+    }
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -16,13 +31,19 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playAudioHandler}
+          className="play"
+          size="2x"
+          icon={isPlaying ? faPause : faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
